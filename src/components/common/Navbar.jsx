d41efe687a56df99eea057829e0452cf15b67af5
= <=>
2024-04-02 23:29:33 +0530

    modified navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -4,19 +4,30 @@ import { AiOutlineQuestionCircle } from "react-icons/ai";
 import { IoSettingsOutline } from "react-icons/io5";
 import { NavLink } from 'react-router-dom';
 import { PATHS } from '../../utils/paths';
+import { FaFireFlameCurved } from 'react-icons/fa6';
 
 const Navbar = () => {
   return (
-    <div className='md:hidden fixed p-1 flex items-center h-16 bg-gray-900 gap-2 w-full'>
-        <NavLink to={PATHS.NOTIFICATION} title='Notification' className='h-full w-full flex items-center justify-center text-white'>
-            <MdNotificationsNone />
+    <div className='md:hidden fixed p-2 flex items-center h-16 bg-gray-900 w-full'>
+      <div>
+        <div className='flex items-center text-green-400 '>
+          <FaFireFlameCurved size={40} />
+          <div>
+            <p className='text-xl font-semibold'>Carbon Cell</p>
+          </div>
+        </div>
+      </div>
+      <div className='flex w-full justify-end gap-6'>
+        <NavLink to={PATHS.NOTIFICATION} title='Notification' className='text-gray-100 h-full'>
+          <MdNotificationsNone size={20} />
         </NavLink>
-        <NavLink  to={PATHS.SUPPORT} title='Support' className='h-full w-full flex items-center justify-center text-white'>
-            <AiOutlineQuestionCircle />
+        <NavLink to={PATHS.SUPPORT} title='Support' className='text-white h-full'>
+          <AiOutlineQuestionCircle size={20} />
         </NavLink>
-        <NavLink  to={PATHS.SETTINGS} title='Settings' className='h-full w-full flex items-center justify-center text-white'>
-            <IoSettingsOutline />
+        <NavLink to={PATHS.SETTINGS} title='Settings' className='text-white h-full'>
+          <IoSettingsOutline size={20} />
         </NavLink>
+      </div>
     </div>
   )
 }
